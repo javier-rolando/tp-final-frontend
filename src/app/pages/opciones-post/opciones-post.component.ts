bf8a36080eb6438e9ee3dbd9e8c8a6472afe8d02
Javier Rolando <javier.rolando98@hotmail.com>
2021-08-08 03:07:14 -0300

    Pagina de opciones para cada post

---
+++ src/app/pages/opciones-post/opciones-post.component.ts
@@ -0,0 +1,184 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Title } from '@angular/platform-browser';
+import { ActivatedRoute } from '@angular/router';
+import { Post } from 'src/app/models/post.model';
+import { FileUploadService } from 'src/app/services/file-upload.service';
+import { PostsService } from 'src/app/services/posts.service';
+
+interface Categoria {
+  value: string;
+  viewValue: string;
+}
+
+@Component({
+  selector: 'app-opciones-post',
+  templateUrl: './opciones-post.component.html',
+  styleUrls: ['./opciones-post.component.css'],
+})
+export class OpcionesPostComponent implements OnInit, OnDestroy {
+  public selectedValue: string;
+  public opcionesPostForm: FormGroup;
+  public post: Post;
+  public imagenTemp: string;
+  private imagenSaved: boolean = false;
+  private postId: string;
+
+  constructor(
+    private fb: FormBuilder,
+    private postsService: PostsService,
+    private fileUploadService: FileUploadService,
+    private activatedRoute: ActivatedRoute,
+    private titleService: Title,
+    private _snackBar: MatSnackBar
+  ) {
+    this.titleService.setTitle('Postinger! | Opciones del post');
+  }
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe(({ id }) => {
+      this.postId = id;
+      this.cargarPost(id);
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.imagenTemp && !this.imagenSaved) {
+      this.fileUploadService
+        .borrarImagenTemp(this.post.usuario._id, 'post', this.imagenTemp)
+        .subscribe(
+          (resp) => {},
+          (err) => {
+            console.log(err);
+          }
+        );
+    }
+  }
+
+  categorias: Categoria[] = [
+    { value: 'Humor', viewValue: 'Humor' },
+    { value: 'Deportes', viewValue: 'Deportes' },
+    { value: 'Info', viewValue: 'Info' },
+    { value: 'Ciencia y educación', viewValue: 'Ciencia y educación' },
+    { value: 'Entretenimiento', viewValue: 'Entretenimiento' },
+    { value: 'Offtopic', viewValue: 'Offtopic' },
+  ];
+
+  cargarPost(id: string) {
+    this.postsService.cargarPostPorId(id).subscribe(
+      (post) => {
+        this.post = post;
+
+        this.selectedValue = this.post.categoria;
+
+        this.opcionesPostForm = this.fb.group({
+          titulo: [
+            this.post.titulo,
+            [
+              Validators.required,
+              Validators.minLength(5),
+              Validators.maxLength(50),
+            ],
+          ],
+          contenido: [
+            this.post.contenido,
+            [
+              Validators.required,
+              Validators.minLength(5),
+              Validators.maxLength(500),
+            ],
+          ],
+          categoria: [this.post.categoria, Validators.required],
+        });
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
+
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action, { duration: 5000 });
+  }
+
+  subirImagen(event: Event) {
+    const input = event.target as HTMLInputElement;
+
+    if (!input.files?.length) {
+      return;
+    }
+
+    this.fileUploadService
+      .subirImagen(input.files[0], 'post')
+      ?.subscribe((resp) => {
+        this.imagenTemp = resp;
+      });
+  }
+
+  actualizarPost() {
+    if (this.opcionesPostForm.invalid) {
+      return;
+    }
+
+    const body = this.opcionesPostForm.value;
+
+    if (this.imagenTemp) {
+      body.imagen = this.imagenTemp;
+    }
+
+    this.postsService.actualizarPost(this.postId, body).subscribe(
+      (resp: any) => {
+        if (body.imagen) {
+          this.imagenSaved = true;
+        }
+        this.openSnackBar(resp.mensaje, 'Aceptar');
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
+
+  campoNoValido(campo: string): boolean {
+    if (this.opcionesPostForm.get(campo)?.invalid) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  getMensajeError(campo: string): string | void {
+    switch (campo) {
+      case 'titulo':
+        if (this.opcionesPostForm.get('titulo')?.hasError('required')) {
+          return 'El título es obligatorio';
+        } else if (this.opcionesPostForm.get('titulo')?.hasError('minlength')) {
+          return 'El título debe tener al menos 5 caracteres';
+        } else if (this.opcionesPostForm.get('titulo')?.hasError('maxlength')) {
+          return 'El título no debe tener más de 50 caracteres';
+        }
+        break;
+      case 'contenido':
+        if (this.opcionesPostForm.get('contenido')?.hasError('required')) {
+          return 'El contenido es obligatorio';
+        } else if (
+          this.opcionesPostForm.get('contenido')?.hasError('minlength')
+        ) {
+          return 'El contenido debe tener al menos 5 caracteres';
+        } else if (
+          this.opcionesPostForm.get('contenido')?.hasError('maxlength')
+        ) {
+          return 'El contenido no debe tener más de 500 caracteres';
+        }
+        break;
+      case 'categoria':
+        if (this.opcionesPostForm.get('categoria')?.hasError('required')) {
+          return 'La categoría es obligatoria';
+        }
+        break;
+      default:
+        return 'Ha ocurrido un error';
+    }
+  }
+}
